e6775edb1066aa5ce2cebb8900d82e97ab28a44e
Connie0914 <solveriddles@gmail.com>
2021-12-03 13:18:53 +0900

    インデックスページをシンプルにして企業HPとログインページへの遷移リンクを設置した。

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,24 @@
 <template>
   <v-app>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <div class="logo py-4 d-flex justify-center">
-          <CompanyLogo />
-        </div>
-        <v-card>
-          <v-card-title class="headline">
-            ベジデジアプリ
-          </v-card-title>
-          <v-card-text>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt saepe magni minima veritatis quae repudiandae. Nobis animi quibusdam excepturi, perspiciatis est in nulla, accusamus ex culpa architecto non nam?</p>
-            <a
-              href="https://tech-goodagri.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              &mdash; Good Agri Technologies
-            </a>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="success"
-              nuxt
-              to="/auth/login"
-            >
-              ログイン画面へ進む
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <CompanyLogo />
+      </v-card-title>
+      <v-card-text>
+      <v-card-actions>
+        <v-row justify="space-between">
+          <v-btn text color="blue-grey" class="white--text" href="https://tech-goodagri.com/">グッドアグリ・テクノロジーズ</v-btn>
+          <v-btn
+            nuxt
+            outlined
+            large
+            to="/auth/login"
+          >
+            ログイン
+          </v-btn>
+        </v-row>
+      </v-card-actions>
+      </v-card-text>
+    </v-card>
   </v-app>
 </template>
